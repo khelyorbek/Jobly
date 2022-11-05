354fceab40c2b38c086ef95e431c0ca0638980f0
khelyorbek <khelyorbek@gmail.com>
2022-11-04 20:14:27 -0400

    updated config so we can use the app on VPS

--- backend/config.js
+++ backend/config.js
@@ -7,13 +7,15 @@ require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
-const PORT = +process.env.PORT || 3001;
+// updated port value so that I can use it in my VPS
+const PORT = 3006;
 
 // Use dev database, testing database, or via env var, production database
+// updated the database url so we can use it on VPS
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
       ? "postgresql://test:test@localhost/jobly_test"
-      : process.env.DATABASE_URL || "postgresql://test:test@localhost/jobly";
+      : `${process.env.DATABASE_URL}/jobly` || "postgresql://test:test@localhost/jobly";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
