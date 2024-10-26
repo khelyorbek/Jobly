7d79e19ffe4d3773f8f079fef53c380961d380d1
Elyorbek Khudaybergenov <khelyorbek@gmail.com>
2024-10-26 18:39:14 -0400

    frontend - configuring Dockerfile with React Production Build and NGINX serve

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://jobly-backend.elyorbek.com";
+const BASE_URL = process.env.BACKEND_URL;
 
 /** API Class.
  *
@@ -104,11 +104,5 @@ class JoblyApi {
   }
 
 }
-
-// below is for debugging purposes only
-// for now, put token ("testuser" / "password" on class)
-// JoblyApi.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
-//   "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
-//   "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
 
 export default JoblyApi;
